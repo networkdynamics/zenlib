4dedcf7f18843220dec5ae15a55911fa64b561af
Derek Ruths <druths@networkdynamics.org>
2012-05-11 16:22:51 -0400

    Updated benchmark figure generation for website

--- src/zen/benchmarks/edgelist.py
+++ src/zen/benchmarks/edgelist.py
@@ -10,7 +10,7 @@ import os
 class NonRandomGraphBenchmark(Benchmark):
 	
 	def __init__(self):
-		Benchmark.__init__(self,'Random Graph Creation')
+		Benchmark.__init__(self,'Loading Edgelist Data')
 		
 		self.NUM_NODES = 1000
 
@@ -20,13 +20,13 @@ class NonRandomGraphBenchmark(Benchmark):
 		fd,fname = tempfile.mkstemp()
 		os.close(fd)
 		
-		self.fname = fname
+		self.fname = fname	
 		
 		# the graph is fully connected
 		fh = open(self.fname,'w')
 		for i in range(self.NUM_NODES):
 			for j in range(i+1,self.NUM_NODES):
-				print >>fh, '%d %d' % (i,j)
+				print >>fh, 'N%d N%d' % (i,j)
 				
 		fh.close()
 		
@@ -36,37 +36,14 @@ class NonRandomGraphBenchmark(Benchmark):
 		# It's taken care of for us
 		pass
 		
-	# def bm_igraph(self):
-	# 	G = ig.Graph()
-	# 	
-	# 	# add nodes
-	# 	node_lookup = {}
-	# 	G.add_vertices(self.NUM_NODES)
-	# 	for i in range(self.NUM_NODES):
-	# 		node_lookup[i] = i
-	# 	
-	# 	# add edges
-	# 	num_nodes = self.NUM_NODES
-	# 	i = 0
-	# 	ni = 0
-	# 	nj = 1
-	# 	edges = []
-	# 	while i < self.NUM_EDGES:
-	# 		edges.append( (ni,nj) )
-	# 		i += 1
-	# 		
-	# 		nj += 1
-	# 		if nj >= num_nodes:
-	# 			ni += 1
-	# 			nj = ni + 1
-	# 	 	
-	# 	G.add_edges(edges)
-		
 	def bm_networkx(self):
 		G = nx.read_edgelist(self.fname)
 		
 	def bm_zen(self):
 		G = zn.edgelist.read(self.fname)
+		
+	def bm_igraph(self):
+		G = ig.read(self.fname,format='ncol',weights=False)
 		
 if __name__ == '__main__':
 	main()
