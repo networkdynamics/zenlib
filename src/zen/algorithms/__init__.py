589175484883450d1208fc181ab1a7837cb8d618
Derek Ruths <druths@networkdynamics.org>
2013-05-26 01:49:16 -0400

    Removed max_weight_matching which isn't in the public source yet

--- src/zen/algorithms/__init__.py
+++ src/zen/algorithms/__init__.py
@@ -3,7 +3,6 @@ from centrality import *
 import clustering
 from components import *
 from matching import *
-from max_weight_matching import *
 from modularity import *
 from properties import *
 from shortest_path import *
