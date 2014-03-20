48c6cbb79cba6608590a6a1021c20669d7d52425
Derek Ruths <druths@networkdynamics.org>
2014-03-20 10:48:58 -0400

    Adding basic control functionality

--- src/zen/exceptions.py
+++ src/zen/exceptions.py
@@ -1,4 +1,15 @@
 
+def type_check(obj,target_type,exception_msg=None):
+	"""
+	Check the type of a given object and raise a TypeError is the type isn't right.
+	"""
+	if type(obj) != target_type:
+		if exception_msg == None:
+			exception_msg = 'Received %s, type %s.  Expected %s' % (str(obj),str(type(obj)),str(target_type))
+		raise TypeError, exception_msg
+		
+	return
+
 class ZenException(Exception):
 	pass
 	
